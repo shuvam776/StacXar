381b51ca87f1d45d09017d3d6711e03913c39153
shuvam_776 <shuvamsatapathi@gmail.com>
2026-02-21 18:08:20 +0530

    more fixes

--- Backend/app.js
+++ Backend/app.js
@@ -7,8 +7,9 @@ const app = express();
 
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
+    origin: process.env.CORS_ORIGIN || true, // 'true' allows the origin of the request if it's not a wildcard
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization', 'user-email']
 }));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
