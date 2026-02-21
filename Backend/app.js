ceee37e968ea9d7eedff3b381f5272d36199aed2
shuvam_776 <shuvamsatapathi@gmail.com>
2026-02-21 19:19:12 +0530

    more fixes

--- Backend/app.js
+++ Backend/app.js
@@ -1,15 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const cloudinary = require('cloudinary').v2;
 const app = express();
 
-
-
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || true, // 'true' allows the origin of the request if it's not a wildcard
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization', 'user-email']
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
 }));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
